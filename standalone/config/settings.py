378f4a42084b3408c5aa694e4df0450c436240a5
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-04-08 20:19:30 -0300

    consetado erro de coletar arquivos estaticos antes de criar o arquivo .env

--- standalone/config/settings.py
+++ standalone/config/settings.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 
 import os
+from dotenv import load_dotenv
+from django.core.exceptions import ImproperlyConfigured
 
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- standalone/entrypoint.sh
+++ standalone/entrypoint.sh
@@ -2,13 +2,15 @@
 # Install extra deps from /opt/extra-deps.txt if it exists
 pip install -r /opt/extra-dependencies.txt
 cd /opt/django-helpdesk/standalone/
-python manage.py migrate --noinput                # Apply database migrations
-python manage.py createsuperuser --noinput
 
 # Starting cron to check emails
 printenv > /etc/env
 env | awk -F= '{printf "export %s=\"%s\"\n", $1, $2}' > /etc/env
 cron &&
+python manage.py migrate --noinput                # Apply database migrations
+python manage.py createsuperuser --noinput
+python manage.py collectstatic --noinput
+
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn standalone.config.wsgi:application \
